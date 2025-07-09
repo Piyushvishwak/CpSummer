d63232b1718dccc763bf62968e20dcbcb478e235
piyush vishwakarma <piyushvish4@gmail.com>
2025-07-09 23:52:15 +0530

    Your message about what changed

--- vjudge3/C.cpp
+++ vjudge3/C.cpp
@@ -22,3 +22,28 @@ int main(){
     cout << count << endl;
     return 0;
 }
+
+
+// //TLE
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define foo ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+// int main(){
+//     foo;
+//     int n, k;
+//     cin >> n >> k;
+//     vector<int> arr(n);
+//     for(int i=0; i<n; i++){
+//         cin >> arr[i];
+//     }
+//     int count=0;
+//     for(int i=0; i<arr.size(); i++){
+//         int sum=0;
+//         for(int j=i; j<arr.size(); j++){
+//             sum+=arr[j];
+//             if(sum==k)  count++;
+//         }
+//     }
+//     cout<<count;
+//     return 0;
+// }
