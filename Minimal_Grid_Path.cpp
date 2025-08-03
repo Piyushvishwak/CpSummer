8fb9afccef0a3e4a829ab4c9c4c1f96e47e41900
piyush vishwakarma <piyushvish4@gmail.com>
2025-08-03 23:37:04 +0530

    found minimal path

--- Minimal_Grid_Path.cpp
+++ Minimal_Grid_Path.cpp
@@ -1,19 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define vi vector<int>
-#define begin v.begin(), v.end()
-#define fastt ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define int long long
-int32_t main(){
-    fastt;
-    int t;
-    cin >> t;
-    vi ans;
-    while(t--){
-        
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<string> g(n);
+    for (auto &row : g) cin >> row;
+
+    vector<pair<int,int>> curr{{0,0}};
+    string ans(1, g[0][0]);
+    vector<vector<bool>> vis(n, vector<bool>(n,false));
+
+    for (int k = 1; k < 2*n-1; k++) {
+        char mn = 'Z' + 1;
+        vector<pair<int,int>> next;
+        // find minimum char
+        for (auto [x,y] : curr) {
+            if (x+1<n) mn = min(mn, g[x+1][y]);
+            if (y+1<n) mn = min(mn, g[x][y+1]);
+        }
+        // collect positions
+        for (auto [x,y] : curr) {
+            if (x+1<n && g[x+1][y]==mn && !vis[x+1][y]) {
+                vis[x+1][y] = true;
+                next.push_back({x+1,y});
+            }
+            if (y+1<n && g[x][y+1]==mn && !vis[x][y+1]) {
+                vis[x][y+1] = true;
+                next.push_back({x,y+1});
+            }
+        }
+        ans += mn;
+        curr.swap(next);
     }
-    for(auto i : ans)  cout << i << endl;
-    return 0;
+    cout << ans;
 }
-// I did it!!
